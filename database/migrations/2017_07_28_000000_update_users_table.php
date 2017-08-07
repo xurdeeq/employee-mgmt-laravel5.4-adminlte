edcfb09df60d7c098215d1e8d09190dd33fd3b08
Sadeeq Akintola <sadeeq2@gmail.com>
2017-08-07 01:08:51 +0100

    Admin Setup Completed

---
+++ database/migrations/2017_07_28_000000_update_users_table.php
@@ -0,0 +1,45 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateUsersTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('users', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('username');
+            $table->string('email')->unique();
+            $table->string('password');
+            $table->string('lastname');
+            $table->string('firstname');
+            $table->integer('role')->nullable()->unsigned();
+            $table->string('phone')->nullable();
+            $table->rememberToken();
+            $table->softDeletes();
+            $table->timestamps();
+
+            $table->foreign('role')->references('id')
+                ->on('roles')->
+                onUpdate('cascade')
+                ->onDelete('cascade');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('users');
+    }
+}
